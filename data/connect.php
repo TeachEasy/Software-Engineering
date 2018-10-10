8193e4384538aa4f5ddc89f226be37ddd54cfa39
Matt Strand <mstrandwest@gmail.com>
2018-10-09 22:54:18 -0400

    Add files via upload

---
+++ data/connect.php
@@ -0,0 +1,20 @@
+<?php
+
+/*Login handled here*/
+session_start();
+
+//these will be changed once we set up the database
+$servername = "localhost";
+$username = "";
+$password = "";
+
+//connect to the database
+$connection = new mysqli($servername, $username, $password);
+
+//Check if the connection was successful
+if($connection->connect_error){
+	die("Connection to the database failed: " . $connection->connect_error);
+} else{
+	echo ("SUCCESS");
+}
+
---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="teachEasyStyle.css">
+</head>
+<body class="loginBackground">
+	<div class="login">
+		<h1>Teach Easy</h1>
+	</div>
+	<h1 id="loginLabel">LOGIN</h1>
+
+	<div class="container">
+		<div class="loginForm">
+			<form method="post" action="data/connect.php">
+				<!-- this only works if it gets clicked on the right...no idea why-->
+				<input type="text" name="username" placeholder="Username" id="inputs">
+				<input type="password" name="password" placeholder="Password" id="inputs">
+				<input type="submit" name="submitLogin" value="Submit" id="submitButton">
+			</form>
+		</div>	
+	</div>
+</body>
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</html>
