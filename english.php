89f7c1c7658203436a25cb26560e71bafd250a94
Matt Strand <mstrandwest@gmail.com>
2018-10-31 12:51:46 -0400

    Add files via upload
    These are the subject pages, and the update of the header to link them together

---
+++ english.php
@@ -0,0 +1,8 @@
+<html>
+	<body>
+	<?php include 'includes/header.php';?>
+
+	English Page
+
+	</body>
+</html>
---
+++ socialstudies.php
@@ -0,0 +1,8 @@
+<html>
+	<body>
+	<?php include 'includes/header.php';?>
+
+	Social Studies
+
+	</body>
+</html>
