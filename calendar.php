25a023deb878b1330e49c29fa0cfde5ed2aa67fc
Makayla Ballenger <32641927+BallengerM@users.noreply.github.com>
2018-10-31 12:52:32 -0400

    Add files via upload

--- calendar.php
+++ calendar.php
@@ -1,94 +1,135 @@
 <html>
+	<head>
+		<link rel="stylesheet" href="components/bootstrap2/css/bootstrap.css">
+		<link rel="stylesheet" href="components/bootstrap2/css/bootstrap-responsive.css">
+		<link rel="stylesheet" href="css/calendar.css">
+	</head>
 	<body>
 	<?php include 'includes/header.php';?>
 
-	<div class="container-fluid">
-		<div class="mainContent">
-			<!--SOURCE: https://www.htmlgoodies.com/tutorials/tables/article.php/3479801/So-You-Want-A-Calendar-Huh.htm-->
-			<TABLE BORDER=3 CELLSPACING=3 CELLPADDING=3 style="width: 90%; margin-left: 5%; height: 50%"> 
-			<TR>
-			<TD COLSPAN="7" ALIGN=center><B>December 2001</B></TD> 
-			</TR>
-			<TR> 
-			<TD COLSPAN="7" ALIGN=center><I>Test Calendar Copied and pasted</I></TD>
-			</TR>
+		<div class="container">
 
-			<TR> 
-				<TD ALIGN=center>Sun</TD>
-				<TD ALIGN=center>Mon</TD>
-				<TD ALIGN=center>Tue</TD>
-				<TD ALIGN=center>Wed</TD>
-				<TD ALIGN=center>Thu</TD>
-				<TD ALIGN=center>Fri</TD>
-				<TD ALIGN=center>Sat</TD>
-			</TR>
 
-			<TR> 
-				<TD ALIGN=center></TD>
-				<TD ALIGN=center></TD>
-				<TD ALIGN=center></TD>
-				<TD ALIGN=center></TD>
-				<TD ALIGN=center></TD>
-				<TD ALIGN=center></TD>
-				<TD ALIGN=center>1</TD>
-			</TR>
+		<div class="page-header">
 
-			<TR> 
-				<TD ALIGN=center>2</TD>
-				<TD ALIGN=center>3</TD>
-				<TD ALIGN=center>4</TD>
-				<TD ALIGN=center>5</TD>
-				<TD ALIGN=center>6</TD>
-				<TD ALIGN=center>7</TD>
-				<TD ALIGN=center>8</TD>
-			</TR>
+			<div class="pull-right form-inline">
+				<div class="btn-group">
+					<button class="btn btn-primary" data-calendar-nav="prev"><< Prev</button>
+					<button class="btn" data-calendar-nav="today">Today</button>
+					<button class="btn btn-primary" data-calendar-nav="next">Next >></button>
+				</div>
+				<div class="btn-group">
+					<button class="btn btn-warning" data-calendar-view="year">Year</button>
+					<button class="btn btn-warning active" data-calendar-view="month">Month</button>
+					<button class="btn btn-warning" data-calendar-view="week">Week</button>
+					<button class="btn btn-warning" data-calendar-view="day">Day</button>
+				</div>
+			</div>
 
-			<TR> 
-				<TD ALIGN=center>9</TD>
-				<TD ALIGN=center>10</TD>
-				<TD ALIGN=center>11</TD>
-				<TD ALIGN=center>12</TD>
-				<TD ALIGN=center>13</TD>
-				<TD ALIGN=center>14</TD>
-				<TD ALIGN=center>15</TD>
-			</TR>
-
-			<TR> 
-				<TD ALIGN=center>16</TD>
-				<TD ALIGN=center>17</TD>
-				<TD ALIGN=center>18</TD>
-				<TD ALIGN=center>19</TD>
-				<TD ALIGN=center>20</TD>
-				<TD ALIGN=center>21</TD>
-				<TD ALIGN=center>22</TD>
-			</TR>
-
-			<TR> 
-				<TD ALIGN=center>23</TD>
-				<TD ALIGN=center>24</TD>
-				<TD ALIGN=center>25</TD>
-				<TD ALIGN=center>26</TD>
-				<TD ALIGN=center>27</TD>
-				<TD ALIGN=center>28</TD>
-				<TD ALIGN=center>29</TD>
-			</TR>
-
-			<TR> 
-				<TD ALIGN=center>30</TD>
-				<TD ALIGN=center>31</TD>
-				<TD ALIGN=center></TD>
-				<TD ALIGN=center></TD>
-				<TD ALIGN=center></TD>
-				<TD ALIGN=center></TD>
-				<TD ALIGN=center></TD>
-			</TR>
-
-			</TABLE>
+			<h3></h3>
+			<small>To see example with events navigate to march 2013</small>
 		</div>
+
+		<div class="row">
+			<div class="span9">
+				<div id="calendar"></div>
+			</div>
+			<div class="span3">
+				<div class="row-fluid">
+					<select id="first_day" class="span12">
+						<option value="" selected="selected">First day of week language-dependant</option>
+						<option value="2">First day of week is Sunday</option>
+						<option value="1">First day of week is Monday</option>
+					</select>
+					<select id="language" class="span12">
+						<option value="">Select Language (default: en-US)</option>
+						<option value="bg-BG">Bulgarian</option>
+						<option value="nl-NL">Dutch</option>
+						<option value="fr-FR">French</option>
+						<option value="de-DE">German</option>
+						<option value="el-GR">Greek</option>
+						<option value="hu-HU">Hungarian</option>
+						<option value="id-ID">Bahasa Indonesia</option>
+						<option value="it-IT">Italian</option>
+						<option value="pl-PL">Polish</option>
+						<option value="pt-BR">Portuguese (Brazil)</option>
+						<option value="ro-RO">Romania</option>
+						<option value="es-CO">Spanish (Colombia)</option>
+						<option value="es-MX">Spanish (Mexico)</option>
+						<option value="es-ES">Spanish (Spain)</option>
+						<option value="es-CL">Spanish (Chile)</option>
+						<option value="es-DO">Spanish (República Dominicana)</option>
+						<option value="ru-RU">Russian</option>
+						<option value="sk-SR">Slovak</option>
+						<option value="sv-SE">Swedish</option>
+						<option value="zh-CN">简体中文</option>
+						<option value="zh-TW">繁體中文</option>
+						<option value="ko-KR">한국어</option>
+						<option value="th-TH">Thai (Thailand)</option>
+					</select>
+					<label class="checkbox">
+						<input type="checkbox" value="#events-modal" id="events-in-modal"> Open events in modal window
+					</label>
+					<label class="checkbox">
+						<input type="checkbox" id="format-12-hours"> 12 Hour format
+					</label>
+					<label class="checkbox">
+						<input type="checkbox" id="show_wb" checked> Show week box
+					</label>
+					<label class="checkbox">
+						<input type="checkbox" id="show_wbn" checked> Show week box number
+					</label>
+				</div>
+
+				<h4>Events</h4>
+				<small>This list is populated with events dynamically</small>
+				<ul id="eventlist" class="nav nav-list"></ul>
+			</div>
+		</div>
+
+		<div class="clearfix"></div>
+		
+
+		<script type="text/javascript" src="components/jquery/jquery.min.js"></script>
+		<script type="text/javascript" src="components/underscore/underscore-min.js"></script>
+		<script type="text/javascript" src="components/bootstrap2/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="components/jstimezonedetect/jstz.min.js"></script>
+		<script type="text/javascript" src="js/language/bg-BG.js"></script>
+		<script type="text/javascript" src="js/language/nl-NL.js"></script>
+		<script type="text/javascript" src="js/language/fr-FR.js"></script>
+		<script type="text/javascript" src="js/language/de-DE.js"></script>
+		<script type="text/javascript" src="js/language/el-GR.js"></script>
+		<script type="text/javascript" src="js/language/it-IT.js"></script>
+		<script type="text/javascript" src="js/language/hu-HU.js"></script>
+		<script type="text/javascript" src="js/language/pl-PL.js"></script>
+		<script type="text/javascript" src="js/language/pt-BR.js"></script>
+		<script type="text/javascript" src="js/language/ro-RO.js"></script>
+		<script type="text/javascript" src="js/language/es-CO.js"></script>
+		<script type="text/javascript" src="js/language/es-MX.js"></script>
+		<script type="text/javascript" src="js/language/es-ES.js"></script>
+		<script type="text/javascript" src="js/language/es-CL.js"></script>
+		<script type="text/javascript" src="js/language/es-DO.js"></script>
+		<script type="text/javascript" src="js/language/ru-RU.js"></script>
+		<script type="text/javascript" src="js/language/sk-SR.js"></script>
+		<script type="text/javascript" src="js/language/sv-SE.js"></script>
+		<script type="text/javascript" src="js/language/zh-CN.js"></script>
+		<script type="text/javascript" src="js/language/cs-CZ.js"></script>
+		<script type="text/javascript" src="js/language/ko-KR.js"></script>
+		<script type="text/javascript" src="js/language/zh-TW.js"></script>
+		<script type="text/javascript" src="js/language/id-ID.js"></script>
+		<script type="text/javascript" src="js/language/th-TH.js"></script>
+		<script type="text/javascript" src="js/calendar.js"></script>
+		<script type="text/javascript" src="js/app.js"></script>
+
+		<script type="text/javascript">
+			var disqus_shortname = 'bootstrapcalendar'; // required: replace example with your forum shortname
+			(function() {
+				var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+				dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+				(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+			})();
+		</script>
 	</div>
-
-
-
 
 	</body>
 </html>
