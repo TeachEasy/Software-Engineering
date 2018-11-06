4c2193cb5775e76d54928d09530a4e8ecd17ad6b
Matt Strand <mstrandwest@gmail.com>
2018-11-06 08:40:55 -0500

    Add files via upload
    Fix login, add an error message, and change color of gradebook option to match other nav bar buttons

--- data/connect.php
+++ data/connect.php
@@ -13,36 +13,36 @@ $_SESSION['loggedIn'] = "0";
 
 //set user and password to values from form
 if ( isset($_POST['username']) && isset($_POST['password']) ) { 
-	$user = $_POST['username'];
+	$_SESSION['user'] = $_POST['username'];
 	$pass = $_POST['password'];
 } else {
 	echo "The values weren't sent";
 }
 
 //connect to the database
-$_SESSION['connection'] = new mysqli($servername, $usernameDB, $passwordDB, $nameDB);
+$connection = new mysqli($servername, $usernameDB, $passwordDB, $nameDB);
 
 //Check if the connection was successful
-if($_SESSION['connection']->connect_error){
-	die("Connection to the database failed: " . $_SESSION['connection']->connect_error);
+if($connection->connect_error){
+	die("Connection to the database failed: " . $connection->connect_error);
 } else{
 	//once the DB is connected, get information from the DB to check the records against the data entered
-	$sqlUser = "SELECT `teacher_username` FROM `teacher` WHERE `teacher_username`='$user'";
+	$sqlUser = "SELECT `teacher_username` FROM `teacher` WHERE `teacher_username`= '{$_SESSION['user']}'";
 	$sqlPass = "SELECT `password` FROM `teacher` WHERE `password`='$pass'";
-	$resultUser = mysqli_query($_SESSION['connection'], $sqlUser);
-	$resultPass = mysqli_query($_SESSION['connection'], $sqlPass);
+	$resultUser = mysqli_query($connection, $sqlUser);
+	$resultPass = mysqli_query($connection, $sqlPass);
 	$textUser = $resultUser->fetch_assoc();
 	$textPass = $resultPass->fetch_assoc();
 
 	
 
 	//check if the user and password match records in the database
-	if($user == $textUser['teacher_username'] && $pass == $textPass['password']){
+	if($_SESSION['user'] == $textUser['teacher_username'] && $pass == $textPass['password']){
 		//get first name and last name to populate the user
-		$sqlUserFirstName = "SELECT `first_name` FROM `teacher` WHERE `teacher_username`='$user'";
-		$sqlUserLastName = "SELECT `last_name` FROM `teacher` WHERE `teacher_username`='$user'";
-		$resultUserFirstName = mysqli_query($_SESSION['connection'], $sqlUserFirstName);
-		$resultUserLastName = mysqli_query($_SESSION['connection'], $sqlUserLastName);
+		$sqlUserFirstName = "SELECT `first_name` FROM `teacher` WHERE `teacher_username`='{$_SESSION['user']}'";
+		$sqlUserLastName = "SELECT `last_name` FROM `teacher` WHERE `teacher_username`='{$_SESSION['user']}'";
+		$resultUserFirstName = mysqli_query($connection, $sqlUserFirstName);
+		$resultUserLastName = mysqli_query($connection, $sqlUserLastName);
 		$_SESSION['userFirstName'] = $resultUserFirstName->fetch_assoc()['first_name'];
 		$_SESSION['userLastName'] = $resultUserLastName->fetch_assoc()['last_name'];
 
@@ -52,7 +52,10 @@ if($_SESSION['connection']->connect_error){
 		//open the calendar if they match
 		echo "<script> window.location.assign('../calendar.php'); </script>";
 	} else{
-		
+		//ADD A WAY TO TELL THE USER THE LOGIN FAILED.
+		echo "<script>alert('Login Failed. Try Again.')</script>";
+		echo "<script> window.location.assign('../index.php'); </script>";
+
 	}
 
 }
