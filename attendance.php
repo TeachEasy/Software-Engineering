10cf38a546fdd5397c3443018ac6847b5362e13d
Makayla Ballenger <32641927+BallengerM@users.noreply.github.com>
2018-11-25 21:12:52 -0500

    Style Changes - Attendance

--- attendance.php
+++ attendance.php
@@ -1,49 +1,52 @@
 <?php include 'includes/header.php';?>
 <html>
 	<body>
-	<?php 
-	//WE SHOULD REALLY BE USING PREPARED STATEMENTS FOR THIS FOR SECURITY, BUT FOR TESTING PURPOSES THIS IS OKAY
 	
-	//set up the database 
-	$servername = "localhost";
-	$usernameDB = "root";
-	$passwordDB = "";
-	$nameDB = "teacheasy";
-	//connect to the database
-	$connection = new mysqli($servername, $usernameDB, $passwordDB, $nameDB);
+
+	<div id="attendance_main">
+			<?php 
+			//WE SHOULD REALLY BE USING PREPARED STATEMENTS FOR THIS FOR SECURITY, BUT FOR TESTING PURPOSES THIS IS OKAY
+			
+			//set up the database 
+			$servername = "localhost";
+			$usernameDB = "root";
+			$passwordDB = "";
+			$nameDB = "teacheasy";
+			//connect to the database
+			$connection = new mysqli($servername, $usernameDB, $passwordDB, $nameDB);
 
 
-	//Create table of students based on teacher
-	$sql = "SELECT s.student_id, CONCAT(s.last_name,', ',s.first_name) AS 'StudentName', group_concat(a.date_absence) AS 'DatesAbsent', COUNT(a.student_id) AS 'TotalAbsent' \n"
-    . "FROM `students` s JOIN `absences` a ON s.student_id = a.student_id \n"
-    . "WHERE s.teacher_id='" . $_SESSION['userId'] . "'GROUP BY s.student_id \n"
-    . "ORDER BY s.last_name ASC;";
+			//Create table of students based on teacher
+			$sql = "SELECT s.student_id, CONCAT(s.last_name,', ',s.first_name) AS 'StudentName', group_concat(a.date_absence) AS 'DatesAbsent', COUNT(a.student_id) AS 'TotalAbsent' \n"
+		    . "FROM `students` s JOIN `absences` a ON s.student_id = a.student_id \n"
+		    . "WHERE s.teacher_id='" . $_SESSION['userId'] . "'GROUP BY s.student_id \n"
+		    . "ORDER BY s.last_name ASC;";
 
-	$result = mysqli_query($connection, $sql) or die("Bad Query: $sql"); 
-	
-	echo"<table>";
-	echo"<tr>
-			<td><b>Student ID</b></td>
-			<td><b>Student Name</b></td>
-			<td><b>Total Absent</b></td>
-			<td><b>Dates Absent</b></td>
-		</tr>";
+			$result = mysqli_query($connection, $sql) or die("Bad Query: $sql"); 
+			
+			echo"<table>";
+			echo"<tr>
+					<td><em><b>Student ID</b></em></td>
+					<td><em><b>Student Name</b></em></td>
+					<td><em><b>Total Absent</b></em></td>
+					<td><em><b>Dates Absent</b></em></td>
+				</tr>";
 
-	while($row = mysqli_fetch_assoc($result)){
-		echo"<tr><td>{$row['student_id']}</td><td>{$row['StudentName']}</td><td>{$row['TotalAbsent']}</td><td>{$row['DatesAbsent']}</td></tr>";
-	}
- 
-    ?>
+			while($row = mysqli_fetch_assoc($result)){
+				echo"<tr><td>{$row['student_id']}</td><td>{$row['StudentName']}</td><td>{$row['TotalAbsent']}</td><td>{$row['DatesAbsent']}</td></tr>";
+			}
+		 
+		    ?>
 
+    <div id="attendance_selection">
+		<!--ADD CSS TO MAKE THESE BUTTONS MATCH THE FORMATTING -->
+    	<button class="navButton" onclick="window.location.href='absences.php'">Take Attendance</button>
+    	<!-- Trigger the modal with a button -->
+		<button type="button" class="navButton" data-toggle="modal" data-target="#myModal">Remove Absence</button>
+		<button type="button" class="navButton" data-toggle="modal" data-target="#singleAddModal">Add Single Absence</button>
+	</div>
 
-
-	<!--ADD CSS TO MAKE THESE BUTTONS MATCH THE FORMATTING -->
-    <button class="navButton" onclick="window.location.href='absences.php'">Take Attendance</button>
-    <!-- Trigger the modal with a button -->
-	<button type="button" class="navButton" data-toggle="modal" data-target="#myModal">Remove Absence</button>
-	<button type="button" class="navButton" data-toggle="modal" data-target="#singleAddModal">Add Single Absence</button>
-
-
+	</div>
 
 
 
