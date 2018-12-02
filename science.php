b0e45a8e6552d7cb6a274b99e0a2aefe1de85e22
Matt Strand <mstrandwest@gmail.com>
2018-12-02 12:03:54 -0500

    Add files via upload
    Remove content editables

--- science.php
+++ science.php
@@ -97,7 +97,6 @@
 		while($row = mysqli_fetch_assoc($resultAssignment)){
 			echo "<td><b>{$row['assignment_name']}</b></td>";
 		}
-		echo "<td><div  id='newAssignmentName' contenteditable='true' placeholder='Add Assignment Name'></div></td>";
 		echo "</tr>";
 
 		//this fills the table with the student information and grades
@@ -115,14 +114,12 @@
 
 		$last_stud = null;
 
-		//add code to number the id for each contenteditable, use https://www.reddit.com/r/webdev/comments/4ga0ug/how_to_save_and_retrieve_contenteditable_data/ to figure out how to get the data from the contenteditables
 		$counter = 0;
 		while($row2 = mysqli_fetch_assoc($result)){
 		    if($last_stud != $row2['Student Name']){
 
 		        // close previous <tr>
 		        if ( $last_stud !== null ) {
-		        	echo "<td><div id='enter".$counter."' contenteditable='true' placeholder='".$last_stud."' data-toggle='tooltip' data-placement='left' title='Enter Grades Here'></div></td>";
 		            echo "</tr>";
 		        }
 
@@ -134,7 +131,6 @@
 		        echo"<td>{$row2['grade']}</td>";
 		    }
 		}
-		echo "<td><div id='enter".$counter."' contenteditable='true' placeholder='".$last_stud."'></div></td>";
 		echo '</tr>';
 	?>
 
